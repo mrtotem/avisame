d322b05989477c1a61a585c3748e13b217057c5a
Octavio Danieli <octaviod@nimble.la>
2016-12-15 00:53:54 -0300

    FIX: danger messages

---
+++ app/src/main/java/com/totem/avisame/network/loaders/GetDangerMessagesLoader.java
@@ -0,0 +1,64 @@
+package com.totem.avisame.network.loaders;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.totem.avisame.models.AlertResponse;
+import com.totem.avisame.models.DangerResponse;
+import com.totem.avisame.models.Message;
+import com.totem.avisame.network.ServiceCatalog;
+import com.totem.avisame.network.base.HttpOperations;
+import com.totem.avisame.network.base.JSONArrayWebServiceLoader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Octavio on 14/12/2016.
+ */
+
+public class GetDangerMessagesLoader extends JSONArrayWebServiceLoader<DangerResponse> {
+
+    private Bundle mArgs;
+    private Context mContext;
+
+    public GetDangerMessagesLoader(Context ctx, Bundle bundle) {
+        super(ctx);
+
+        this.mArgs = bundle;
+    }
+
+    @Override
+    protected DangerResponse processResponse(JSONArray response) {
+        List<Message> dangers = new ArrayList<>();
+
+        for (int i = 0; i < response.length(); i++) {
+            try {
+                dangers.add(new Gson().fromJson(response.get(i).toString(), Message.class));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return new DangerResponse(dangers);
+    }
+
+    @Override
+    protected HttpOperations getHTTPMethod() {
+        return HttpOperations.GET;
+    }
+
+    @Override
+    protected String getPath() {
+        return ServiceCatalog.DANGER + "/" + mArgs.getString("email");
+    }
+
+    @Override
+    protected void onInvalidToken() {
+
+    }
+}
---
+++ app/src/main/java/com/totem/avisame/network/loaders/UpdateDangerMessageLoader.java
@@ -0,0 +1,66 @@
+package com.totem.avisame.network.loaders;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.totem.avisame.application.AppSettings;
+import com.totem.avisame.models.Message;
+import com.totem.avisame.network.ServiceCatalog;
+import com.totem.avisame.network.base.HttpOperations;
+import com.totem.avisame.network.base.JSONWebServiceLoader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Octavio on 03/12/2016.
+ */
+public class UpdateDangerMessageLoader extends JSONWebServiceLoader<Message> {
+
+    private Bundle mArgs;
+    private Context mContext;
+
+    public UpdateDangerMessageLoader(Context ctx, Bundle args) {
+        super(ctx);
+
+        this.mContext = ctx;
+        this.mArgs = args;
+    }
+
+    @Override
+    protected Message processResponse(JSONObject response) throws JSONException {
+        return new Gson().fromJson(response.toString(), Message.class);
+    }
+
+    @Override
+    protected HttpOperations getHTTPMethod() {
+        return HttpOperations.PUT;
+    }
+
+    @Override
+    protected String getPath() {
+        return ServiceCatalog.USERS + "/" + AppSettings.getUser().getId() + ServiceCatalog.DANGER + "/" + mArgs.getString("message-id");
+    }
+
+    @Override
+    protected JSONObject getRequestData() {
+
+        JSONObject data = new JSONObject();
+        try{
+            data.put("date", mArgs.getString("date"));
+            data.put("message", mArgs.getString("message"));
+            data.put("lat", mArgs.getString("lat"));
+            data.put("long", mArgs.getString("long"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+    @Override
+    protected void onInvalidToken() {
+
+    }
+}
